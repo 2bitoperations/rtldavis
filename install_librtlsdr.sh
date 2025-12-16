0251730443c822108261bf2b4632794d01cc251c
Andrew Malota <2bitoperations@gmail.com>
2025-12-15 19:06:02 -0600

    still trying for working on a pi

--- install_librtlsdr.sh
+++ install_librtlsdr.sh
@@ -10,6 +10,9 @@ if [ "$EUID" -ne 0 ]; then
   exit 1
 fi
 
+echo "Removing system librtlsdr packages to avoid conflicts..."
+apt-get remove -y librtlsdr-dev librtlsdr0 || true
+
 echo "Installing build dependencies..."
 apt-get update
 apt-get install -y git cmake build-essential libusb-1.0-0-dev pkg-config
@@ -17,6 +20,7 @@ apt-get install -y git cmake build-essential libusb-1.0-0-dev pkg-config
 echo "Cloning rtl-sdr-blog repository..."
 cd /tmp
 if [ -d "rtl-sdr-blog" ]; then
+    echo "Removing existing rtl-sdr-blog directory..."
     rm -rf rtl-sdr-blog
 fi
 git clone https://github.com/rtlsdrblog/rtl-sdr-blog.git
@@ -25,11 +29,12 @@ cd rtl-sdr-blog
 echo "Building librtlsdr..."
 mkdir build
 cd build
-cmake ../ -DINSTALL_UDEV_RULES=ON
+cmake ../ -DINSTALL_UDEV_RULES=ON -DDETACH_KERNEL_DRIVER=ON
 make
 
 echo "Installing librtlsdr..."
 make install
+ldconfig
 
 echo "Configuring library path..."
 echo "/usr/local/lib" > /etc/ld.so.conf.d/rtlsdr.conf
@@ -39,7 +44,4 @@ echo "Blacklisting kernel modules..."
 cp ../rtl-sdr.rules /etc/udev/rules.d/
 echo 'blacklist dvb_usb_rtl28xxu' > /etc/modprobe.d/blacklist-rtl.conf
 
-echo "Done!"
-echo "The old librtlsdr package can sometimes cause conflicts."
-echo "It is recommended to remove it with: apt remove librtlsdr0"
-echo "Please reboot your system for all changes to take effect."
+echo "Done! Please reboot your system for all changes to take effect."
